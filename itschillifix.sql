b9485bdbb97cd5f2b23308878fcb9625467de047
DESKTOP-K6OO5R5\Windows-10 <venatafatmala@gmail.com>
2017-05-29 11:36:28 +0700

    no message

--- itschillifix.sql
+++ itschillifix.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 29, 2017 at 06:13 AM
+-- Generation Time: May 29, 2017 at 06:35 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 

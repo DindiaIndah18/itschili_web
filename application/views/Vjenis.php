01bd99ec2e3a7f60064e7956184d807d3285d6a0
DESKTOP-K6OO5R5\Windows-10 <venatafatmala@gmail.com>
2017-05-31 20:59:48 +0700

    - insyaallah fix jenis

--- application/views/Vjenis.php
+++ application/views/Vjenis.php
@@ -65,13 +65,9 @@ $this->load->view('header');
                                                   <td><?php echo $data->id_jenis;?></td>
                                                   <td><?php echo $data->nama_jenis;?></td>
                                                   <td class="text-right">
-<<<<<<< HEAD
                                                       <a href="<?php echo base_url('index.php/itschilli/menuJenisCabaiEdit/'.$data->id_jenis); ?>" class="btn btn-simple btn-warning btn-icon edit"><i class="material-icons">dvr</i></a>
                                                       <a href="<?php echo base_url('index.php/itschilli/deletejenis/'.$data->id_jenis); ?>" class="btn btn-simple btn-danger btn-icon remove"><i class="material-icons">close</i></a>
-=======
-                                                      <a href="<?php echo base_url('index.php/itschilli/menuJenisCabai/'.$data->id_jenis); ?>" class="btn btn-simple btn-warning btn-icon edit"><i class="material-icons">dvr</i></a>
-                                                      <a href="<?php echo base_url('index.php/itschilli/delete/'.$data->id_jenis); ?>" class="btn btn-simple btn-danger btn-icon remove"><i class="material-icons">close</i></a>
->>>>>>> 603f27282a9461ed2144743a2cafca2a75757ef4
+                                                      
                                                   </td>
                                               </tr>
                                               <?php } ?>
